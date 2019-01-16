247a94d1d95e677e4559e5c4dcbb5fa48edcd165
blue32captain <blue32captain@gmail.com>
2019-01-16 12:35:03 +0300

    add react hot loader

--- bin/server/server.js
+++ bin/server/server.js
@@ -8,20 +8,20 @@ import webpackMiddleware from 'webpack-dev-middleware'
 import webpackConfig from '../../webpack'
 
 
-const port      = 9001
-const app       = express()
-const compiler  = webpack(webpackConfig)
-const debug     = _debug('app:bin:server')
+const port      = 9001;
+const app       = express();
+const compiler  = webpack(webpackConfig);
+const debug     = _debug('app:bin:server');
 
-app.disable('x-powered-by')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json({ strict: true, limit: '10mb' }))
-app.use(historyApiFallback())
-app.use(webpackMiddleware(compiler, webpackConfig.devServer))
+app.disable('x-powered-by');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ strict: true, limit: '10mb' }));
+app.use(historyApiFallback());
+app.use(webpackMiddleware(compiler, webpackConfig.devServer));
 
 app.listen(port, 'localhost', (err) => {
   if (err) {
     debug(chalk.red(err))
   }
   debug('Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port)
-})
+});
--- src/containers/Root.jsx
+++ src/containers/Root.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
+import { hot } from 'react-hot-loader/root'
+
 import App from './App'
 import routes from './Router'
 import ScrollToTop from './ScrollToTop'
 
 
-export default class Root extends React.PureComponent {
+class Root extends React.PureComponent {
 
   render() {
     const { store } = this.props;
@@ -25,3 +27,5 @@ export default class Root extends React.PureComponent {
     )
   }
 }
+
+export default hot(Root)
