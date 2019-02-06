624ccb861efaaf718b9a993bde52347001394d0c
blue32captain <blue32captain@gmail.com>
2019-02-06 13:47:45 +0300

    destructuring render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import store from 'redux/store'
 
 import Root from 'containers/Root'
 
 
-ReactDOM.render(
+render(
   <Root store={store} />,
   document.getElementById('root')
 );
